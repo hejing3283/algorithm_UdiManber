cd4ac5a83e96eaf2c4257448a1088a5fca1e43b1
moonhj00 <violet.hj@gmail.com>
2015-07-11 21:16:32 -0400

    init

---
+++ skyline.py
@@ -0,0 +1,13 @@
+def skyline( coord ) :
+  '''
+  input a series of locations for rectangeles, 
+  output the locations for the skylines eliminating the inner lines
+  input: 
+  (1,11,5), (2,6,7), (3,13,9), (12,7,16),(14,3,25), 
+  (19,18,22), (23,13,29), (24,4,28)
+  output :
+  (1,11,3,13,9,0,12,7,16,3,19,18,22,3,23,13,29,0)
+
+  '''
+
+  return skyline
