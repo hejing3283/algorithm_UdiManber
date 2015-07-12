d852e0a7f24bebf7e1a6d215517b95076460c7d8
moonhj00 <violet.hj@gmail.com>
2015-07-11 21:05:19 -0400

    init maximal induced subgraph

---
+++ maximal_subgraph.py
@@ -0,0 +1,10 @@
+def maximal_induced_subgraph( G, k ):
+  '''
+  Given a graph G = (V, E), find a subgraph S, in which, 
+  all vertices in S have degree >=k, or return None if no S identified 
+  Sol1: identify those not belongs to S
+  input: adjcency list G, and degree cutoff k
+  output: subgraph S 
+  '''
+  return S
+  
