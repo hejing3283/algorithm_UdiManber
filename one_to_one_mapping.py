7d5466f04f23e34ac954d8fc58b5047c02fdab3f
moonhj00 <violet.hj@gmail.com>
2015-07-11 21:11:02 -0400

    init one-to-one mapping

---
+++ one_to_one_mapping.py
@@ -0,0 +1,11 @@
+def mapping( f, n ) :
+  '''
+  given a set of integers f, between 1 to n, 
+  identify a subset of S in which all the elements in S, 
+  are mapping to all the elements in S
+  input: a set of integers f, maximum integer n  
+  output: new set S
+  '''
+  A = range(1, n+1)
+  return S
+
