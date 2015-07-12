32d720087f0a057906a8eea2e2e51ceb22b14f3d
moonhj00 <violet.hj@gmail.com>
2015-07-11 21:12:59 -0400

    init

---
+++ celebrity.py
@@ -0,0 +1,8 @@
+def celebrity( Know ) :
+  '''
+  Given a adjacency matrix indicating the know relationship 
+  btw n persons, identify the celebrity in this graph, 
+  or return None
+  '''
+  return celeb
+
