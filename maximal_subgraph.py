49692ea25cb0947a67607e9f49440fecf3fadc4e
moonhj00 <violet.hj@gmail.com>
2015-07-13 21:24:41 -0400

    k-core maximal subgraph finished

--- maximal_subgraph.py
+++ maximal_subgraph.py
@@ -6,5 +6,35 @@ def maximal_induced_subgraph( G, k ):
   input: adjcency list G, and degree cutoff k
   output: subgraph S 
   '''
+  S = G.copy()
+  nodes = set( S.keys() )
+  
+  for key, neig in S.items():
+    neig = set(neig).intersection(nodes)
+    if len(neig) < k :
+      S.pop(key, None)
+      nodes.remove( key ) 
+  ### update final k-cores subgraph 
+  for key, neig in S.items():
+    S[ key ] = list(set(neig).intersection(nodes))
   return S
   
+def prepG( ) :
+  G = {1: [2,3,4,5],
+       2: [1,3,4,5],
+       3: [1,2,4],
+       4: [1,2,3,5,6,7],
+       5: [2,4],
+       6: [4],
+       7: [4, 5]
+       }
+  return G 
+
+G = prepG()
+print "orignal nodes", G.keys(), G.values()
+for k in range(2,6):
+  S = maximal_induced_subgraph(G, k)
+  print k, "-core with nodes ", S.keys(), S.values()
+
+
+
