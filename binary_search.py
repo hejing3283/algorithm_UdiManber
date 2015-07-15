f926e6e55dd371f340ed1c56ab748371edc3aa6f
moonhj00 <violet.hj@gmail.com>
2015-07-14 20:37:28 -0400

    init

---
+++ binary_search.py
@@ -0,0 +1,7 @@
+def binary_search(A, n, z) :
+  '''
+  input: sorted array A in the range of 1 to n, search key z
+  output: index of z in A, or None if not in A
+  desp: recursive version 
+  '''
+  return idxZ 
---
+++ binary_search_cyclic.py
@@ -0,0 +1,8 @@
+def cyclic_binary_search(X, n, z):
+  '''
+  input: X, a cyclically sorted array in range 1 to n
+  output: position of the mininal element
+  desp.: in recursion
+  '''
+
+  return pos 
