cf2c3bcb5e017b0708215f8f38e3da7a8d654f53
moonhj00 <violet.hj@gmail.com>
2015-07-14 20:01:38 -0400

    init

---
+++ knapsack.py
@@ -0,0 +1,11 @@
+def knapsack(S, K) :
+  '''
+  dynamic programming to solve knapsack problem
+  input: S, an array of size n storing sizes of the items
+         K, the size of the knapsack
+  output: P ( a 2-d array such that
+  P[i, k].exist = True if there exist a solution to the problem with the first i elements  ## a knapsack of size k, 
+  and P[i,k].belong = true if the ith element belongs to that solution
+  '''
+  return P
+
---
+++ maximum_consecutive_sequence.py
@@ -0,0 +1,8 @@
+def maximum_consecutive_sequence( X ):
+  '''
+  Given an array of n intergers, not necessarily positive,
+  find the largest sum of a consecutive seq, if sum = negative, assign it 0.
+  input : An array of size n
+  output: global_max( the sum the the maixumum subsequence
+  '''
+  return gMax
