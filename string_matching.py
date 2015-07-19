2a11cc17fbf4082368353642cba25687db8721de
moonhj00 <violet.hj@gmail.com>
2015-07-18 23:06:31 -0400

    string matching init

---
+++ string_matching.py
@@ -0,0 +1,25 @@
+def string_match(A, n, B, m):
+  '''
+  input: A, a string of size n, 
+	B , a string a size m
+  output: Start, as the fisrt index such taht B is a subtring of A starting at A[Start]
+  '''
+  return start
+
+A = "xyxxyxyxyyxyxyxyyxyxyxx"
+B = "xyxyyxyxyxx"
+def compute_nextB(B, m):
+  '''
+  preprocess the talbe for next 
+  brutal-force way
+  '''
+  nextB = [-1,0]
+  iB = 0
+  for im in range(1,m+1):
+    while im > 0 :
+      if im  
+    
+    
+  
+  return nextB
+nextB = 
