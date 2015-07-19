1dea98d63aa440d469c3c30c99c8ec5f944bbf46
moonhj00 <violet.hj@gmail.com>
2015-07-18 21:07:45 -0400

    init huffman encoding

---
+++ huffman_encoding.py
@@ -0,0 +1,8 @@
+def huffman_encoding(C, F):
+  '''
+  given an array and character and their freuqency,
+  output the huffman encoding for those frequence
+
+  TODO: extension: given a file, compress it using huffman's encoding 
+  '''
+  return heap 
