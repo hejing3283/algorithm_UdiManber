c26c032aad6780b96fdc9f40277c445e55d43b22
moonhj00 <violet.hj@gmail.com>
2015-07-18 23:35:45 -0400

    init DP sequencing_comparision

---
+++ sequence_comparison.py
@@ -0,0 +1,8 @@
+def sequence_comparison( A, B) :
+  '''
+  dynamic programm to find the minium editing distance 
+  of string B to become string A
+  input:  string A, string B
+  output:  minimum edition distance ,adn the editing
+  '''
+  return d, seqE 
