60b0ea264a5d4f4a849d83d6d842885925ce7f2c
moonhj00 <violet.hj@gmail.com>
2015-07-19 23:37:41 -0400

    debugging

--- one_to_one_mapping.py
+++ one_to_one_mapping.py
@@ -8,7 +8,26 @@ def mapping( A , f ) :
   thougth:
   elimination of elements that not belongs to S
   '''
-  A = range(1, n+1)
+  n = len(A)
+  fA = map(f, A)
+  fA = [i for i in fA if i in A]
+
+  S = A[:]
+  counter = [0 for _ in range(n) ] 
+  for i in range(len(fA)) : 
+    counter[ fA[i] ] +=  1 
+  queue = [ j for j in range(n) if counter[j] == 0 ] 
+  print counter
+  while len(queue) > 0 :
+    print queue, 
+    i = queue.pop(0)
+    print i,
+    S.pop(i)
+    counter[ fA[i] ] = counter[ fA[i] ] - 1
+    print counter
+    if counter[ fA[i] ] == 0:
+      queue.extend( [fA[i]] ) 
+
   return S
 
 A = [1,5,2,3,7,8,10]
@@ -16,4 +35,6 @@ A = [1,5,2,3,7,8,10]
 def mapf_1(x):
   return 2 * x -1
 
+fun = mapf_1
+print mapping(A, fun) ### do not pass the name of the function, pass the function 
 
