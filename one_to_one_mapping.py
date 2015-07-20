ca0e9a6759dbc525a6a53cc0ca0cf8edf0b24f53
moonhj00 <violet.hj@gmail.com>
2015-07-19 23:11:03 -0400

    update one to one mapping

--- one_to_one_mapping.py
+++ one_to_one_mapping.py
@@ -1,11 +1,19 @@
-def mapping( f, n ) :
+def mapping( A , f ) :
   '''
-  given a set of integers f, between 1 to n, 
+  given a set of integers A, and a function f 
   identify a subset of S in which all the elements in S, 
   are mapping to all the elements in S
   input: a set of integers f, maximum integer n  
   output: new set S
+  thougth:
+  elimination of elements that not belongs to S
   '''
   A = range(1, n+1)
   return S
+
+A = [1,5,2,3,7,8,10]
+
+def mapf_1(x):
+  return 2 * x -1
+
 
