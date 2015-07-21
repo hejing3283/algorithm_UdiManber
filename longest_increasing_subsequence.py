925db782603f76ea2bb098f656c8c3d181bb2309
moonhj00 <violet.hj@gmail.com>
2015-07-20 22:52:58 -0400

    init LIS

---
+++ longest_increasing_subsequence.py
@@ -0,0 +1,10 @@
+def longest_increasing_subsequencing( A , m ) :
+  ''' 
+  input: a sequence of integers
+  output : the longest increasing subsequence 
+  '''
+  return LIS 
+
+if _name_ = "__main__" :
+
+  print longest_increasing_subsequencing(A, m)
