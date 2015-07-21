2111fda5ff140c7805464739d387477c69373b76
moonhj00 <violet.hj@gmail.com>
2015-07-20 21:19:04 -0400

    init minimum edit distance

---
+++ minimum_edit_distance.py
@@ -0,0 +1,9 @@
+def minimum_edit_distance(A, n, B, m):
+  '''
+  input: given string A, with length n and string B with length m
+  compute the minium edit distance from A to B
+  output:  the cost matrix
+  '''
+
+  return C
+
