507770b512bb7d7dce8f55618a2218b6bbc22324
moonhj00 <violet.hj@gmail.com>
2015-07-20 22:13:14 -0400

    init coloring_problem.py

---
+++ coloring_problem.py
@@ -0,0 +1,7 @@
+def coloring_problem(S, k, r):
+  '''
+  input: set S of distinct element Si, with is or r element for each , satisfy k <= 2^(r-2)
+  question: color S to satisfying that any subset of Si contains at leat 2 distinct colors
+  output: coloring schema 
+  '''
+  return cS 
