ff47d59697d9206e352e90c749a03e5c82d03bfb
moonhj00 <violet.hj@gmail.com>
2015-07-23 09:31:28 -0400

    init mpre

--- depth_first_search.py
+++ depth_first_search.py
@@ -6,4 +6,19 @@ def depth_first_search(G,v):
 	  total visitied vertices
   '''
 
+def dfs_numbering(G, v):
+  '''
+  output : for every vertex v, v.DFS is set to the DFS number of v.
+  '''
+  return G
   
+def build_DFS_tree(G, v):
+  '''
+  given a graph
+  output: a DFS tree
+  '''
+  return 
+
+def depth_first_search_nonrecursive(G, v) :
+
+  return G
