0f1d0fa8880941b5b52188e3820b76ece88d93a4
moonhj00 <violet.hj@gmail.com>
2015-07-29 23:22:36 -0400

    init

---
+++ dijkstra_algorithm.py
@@ -0,0 +1,7 @@
+def single_source_shortest_path(G, v):
+  '''
+  find the shrotest path of a graph
+  input: G = (V,E) a weighted directed graph, and v ( the source vertex) 
+  output: for each vertex w, w.SP is the length of shortest path from v to w
+  '''
+  return 
